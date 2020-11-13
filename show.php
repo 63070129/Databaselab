307a544f0a347da27859d24b01ad4244e3c06817
63070129 <73679698+63070129@users.noreply.github.com>
2020-11-13 10:36:33 +0700

    Add files via upload

--- show.php
+++ show.php
@@ -5,7 +5,7 @@
 <body>
 <?php
 $conn = mysqli_init();
-mysqli_real_connect($conn, 'labpc129.mysql.database.azure.com', 'it63070129@labpc129', '029154897zZ', 'itflab', 3306);
+mysqli_real_connect($conn, 'labpc129.mysql.database.azure.com', 'it63070129@labpc', '123456789', 'itflab', 3306);
 if (mysqli_connect_errno($conn))
 {
     die('Failed to connect to MySQL: '.mysqli_connect_error());
