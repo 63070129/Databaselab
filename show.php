21100fb501cf21a6bc31ac8f51c0a0083ca3804b
63070129 <73679698+63070129@users.noreply.github.com>
2020-11-17 09:49:52 +0700

    Update show.php

--- show.php
+++ show.php
@@ -30,9 +30,9 @@ while($Result = mysqli_fetch_array($res))
 {
 ?>
   <tr>
-    <td><?php echo $Result['Name'];?></div></td>
-    <td><?php echo $Result['Comment'];?></td>
-    <td><a href="delete.php?id=<?php echo $data['id']; ?>">ลบ</a>   <a href="edit.php?id=<?php echo $data['id']; ?>">แก้ไข</a></td>
+    <td><div align="center"><?php echo $Result['Name'];?></div></td>
+    <td><div align="center"><?php echo $Result['Comment'];?></td>
+    <td><div align="center"><a href="delete.php?id=<?php echo $data['id']; ?>">ลบ</a>   <a href="edit.php?id=<?php echo $data['id']; ?>">แก้ไข</a></td>
   </tr>
 <?php
 }
