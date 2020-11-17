d055a49b9425cde003d0f38473245ad204339147
63070129 <73679698+63070129@users.noreply.github.com>
2020-11-17 10:34:14 +0700

    Add files via upload

--- Delete.php
+++ Delete.php
@@ -2,18 +2,12 @@
 
 $conn = mysqli_init();
 mysqli_real_connect($conn, 'labpc129.mysql.database.azure.com', 'it63070129@labpc129', '029154897zZ', 'itflab', 3306);
-if (mysqli_connect_errno($conn))
-{
-    die('Failed to connect to MySQL: '.mysqli_connect_error());
-}
-
 
 $name = $_POST['name'];
 $comment = $_POST['comment'];
 $link = $_POST['link'];
 
-
-$sql = "DELETE INTO guestbook (Name , Comment , Link) VALUES ('$name', '$comment', '$link')";
+$sql = "DELETE FROM guestbook where id='$id'";
 
 
 if (mysqli_query($conn, $sql)) {
