221ab3d8c2d3beb26a81663856fa7306ffd11251
63070129 <73679698+63070129@users.noreply.github.com>
2020-11-19 13:38:29 +0700

    Update delete.php

--- delete.php
+++ delete.php
@@ -4,12 +4,12 @@ $conn = mysqli_init();
 mysqli_real_connect($conn, 'labpc129.mysql.database.azure.com', 'it63070129@labpc129', '029154897zZ', 'itflab', 3306);
 
 $id = $_GET['ID'];
-$delete = "DELETE FROM Guestbook WHERE  id=$id";
-if ($conn->query($delete) === TRUE) {
+$delete = "DELETE FROM Guestbook WHERE id=$id";
+if (mysqli_query($conn, $delete)) {
   echo "Record deleted successfully";
 } else {
-  echo "Error deleting record: " . $conn->error;
+  echo "Error deleting record: " . mysqli_error($conn);
 }
 
-$conn->close();
+mysqli_close($conn);
 ?>
