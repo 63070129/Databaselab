ec69cf04d5a3549f9d876e5c325d47cd671e8bc0
63070129 <73679698+63070129@users.noreply.github.com>
2020-11-19 12:38:14 +0700

    Update Delete.php

--- Delete.php
+++ Delete.php
@@ -3,20 +3,6 @@
 $conn = mysqli_init();
 mysqli_real_connect($conn, 'labpc129.mysql.database.azure.com', 'it63070129@labpc129', '029154897zZ', 'itflab', 3306);
 
-$name = $_POST['Name'];
-$comment = $_POST['Comment'];
 $id = $_GET['ID'];
-$link = $_POST['link'];
-$del = "DELETE FROM Guestbook Where id = $id;
-
-if($del)
-{
-    mysqli_close($con);
-    header("location:show.php");
-    exit;	
-}
-else
-{
-    echo "Error deleting record";
-}
+"DELETE FROM Guestbook Where id = $id;
 ?>
