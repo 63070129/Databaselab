c5513aa196ee5ae77d509f81851db184b4a1254e
63070129 <73679698+63070129@users.noreply.github.com>
2020-11-20 12:50:54 +0700

    Add files via upload

---
+++ update.php
@@ -0,0 +1,21 @@
+<?php
+
+$conn = mysqli_init();
+mysqli_real_connect($conn, 'labpc129.mysql.database.azure.com', 'it63070129@labpc129', '029154897zZ', 'itflab', 3306);
+if (mysqli_connect_errno($conn))
+{
+    die('Failed to connect to MySQL: '.mysqli_connect_error());
+}
+
+
+$name = $_POST['name'];
+$comment = $_POST['comment'];
+$sql = "UPDATE guestbook SET (Name , Comment) VALUES ('$name', '$comment')" WHERE id = $id" ;
+if (mysqli_query($conn, $sql)) {
+    echo "Update record successfully";
+  } else {
+    echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+  }
+  
+mysqli_close($conn);
+?>
