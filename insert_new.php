f86c87b6526b5efbe04c695d4a6eb2f1f7aa93c8
63070129 <73679698+63070129@users.noreply.github.com>
2020-11-20 14:36:58 +0700

    Add files via upload

--- insert_new.php
+++ insert_new.php
@@ -1,24 +1,27 @@
+<html>
+<head>
+<title>Editor</title>
+</head>
 <?php
-
 $conn = mysqli_init();
 mysqli_real_connect($conn, 'labpc129.mysql.database.azure.com', 'it63070129@labpc129', '029154897zZ', 'itflab', 3306);
 if (mysqli_connect_errno($conn))
 {
     die('Failed to connect to MySQL: '.mysqli_connect_error());
 }
-
-$id = $_GET['ID']
-$name = $_POST['name'];
-$comment = $_POST['comment'];
-$sql = "UPDATE tb_member SET  
-			Name ='$name' ,
-			Comment ='$comment' ,
-            WHERE id='$id' ";
+$id = mysqli_real_escape_string($con,$_GET['ID']);
+$sql = "SELECT * FROM guestbook WHERE id='$id' ";
+?>
+    <form action = "update.php" method = "get" id="CommentForm" >
+    <input type="text" name="name" value="<?php echo $Result['name'];?>">
+    <textarea rows="10" cols="20" name ="comment" value="<?php echo $Result['name'];?>">
+<?php
 if (mysqli_query($conn, $sql)) {
     echo "Update record successfully";
   } else {
     echo "Error: " . $sql . "<br>" . mysqli_error($conn);
   }
-  
+
 mysqli_close($conn);
+
 ?>
