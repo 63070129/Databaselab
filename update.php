e57d4d8d15e9dbd1527fc7db288be879a7acfa48
63070129 <73679698+63070129@users.noreply.github.com>
2020-11-20 21:10:14 +0700

    Update update.php

--- update.php
+++ update.php
@@ -17,3 +17,26 @@ if (mysqli_query($conn, $sql)) {
   
 mysqli_close($conn);
 ?>
+$conn = mysqli_init();
+mysqli_real_connect($conn, 'labpc129.mysql.database.azure.com', 'it63070129@labpc129', '029154897zZ', 'itflab', 3306);
+if (mysqli_connect_errno($conn))
+{
+    die('Failed to connect to MySQL: '.mysqli_connect_error());
+}
+$res = mysqli_query($conn, 'SELECT * FROM guestbook');
+?>
+<html>
+<head>
+	<title>Editor</title>
+</head>
+<body>
+    <form action ="update.php" method = "post" id="CommentForm" >
+    Name:<br>
+    <input type="text" name = "name" id="idName" value=<?php echo " $Result['Name'];"?>><br>
+    Comment:<br>
+    <textarea rows="10" cols="20" name = "comment" id="idComment"value="<?php echo  $Result['comment'];?></textarea><br>  
+    <input type="submit" id="commentBtn">
+  </form>
+</body>
+</html>
+
