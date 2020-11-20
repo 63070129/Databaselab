392258434a9269efbac8d96d70c923f39c50c566
63070129 <73679698+63070129@users.noreply.github.com>
2020-11-20 21:50:38 +0700

    Update show.php

--- show.php
+++ show.php
@@ -1,46 +1,67 @@
+<!DOCTYPE html>
 <html>
 <head>
-<title>ITF Lab</title>
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
-  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
-  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+	<title>Show Data</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous"">
+	<style type="text/css">
+		.card {
+			border-top: solid 5px #37cf23;
+		}
+	</style>
 </head>
-<body>
-<div class="container">
-<?php
-$conn = mysqli_init();
-mysqli_real_connect($conn, 'labpc129.mysql.database.azure.com', 'it63070129@labpc129', '029154897zZ', 'itflab', 3306);
-if (mysqli_connect_errno($conn))
-{
-    die('Failed to connect to MySQL: '.mysqli_connect_error());
-}
-$res = mysqli_query($conn, 'SELECT * FROM guestbook');
-?>
-<table width="600" border="1" table class="table table-dark table-striped">
-  <tr>
-    <th width="100"> <div align="center">Name</div></th>
-    <th width="350"> <div align="center">Comment </div></th>
-    <th width="150"> <div align="center">Action </div></th>
-  </tr>
-<?php
-while($Result = mysqli_fetch_array($res))
-{
-?>
-  <tr>
-    <td><?php echo $Result['Name'];?></div></td>
-    <td><?php echo $Result['Comment'];?></td>
-    <td><a href="delete.php?ID=<?php echo $Result['ID'];?>" class="btn btn-sm btn-primary">ลบ</a> 
-        <a href="edit.php?ID=<?php echo $Result['ID'];?>" class="btn btn-sm btn-primary">แก้ไข</a></td>
-  </tr>
-<?php
-}
-?>
-</table>
-<?php
-mysqli_close($conn);
-?>
+<body class="bg-light py-5">
+	<div class="container text-dark">
+		<div class="row">
+			<div class="col-12 col-lg-8 offset-lg-2">
+				<div class="card shadow">
+					<div class="card-body">
+						<div class="row">
+							<div class="col-6"><h1 class="text-monospace">Data</h1></div>
+							<div class="col-6 text-right"><a href="form.html" class="btn btn-success btn-sm font-weight-bold">ADD</a></div>
+						</div>
+						<div class="table-success">
+							<?php
+								$conn = mysqli_init();
+								mysqli_real_connect($conn, 'serveritf.mysql.database.azure.com', 'jiraphat@serveritf', 'Jira2545', 'ITFLab', 3306);
+								if (mysqli_connect_errno($conn))
+								{
+								    die('Failed to connect to MySQL: '.mysqli_connect_error());
+								}
+								$res = mysqli_query($conn, 'SELECT * FROM guestbooks');
+							?>
+							<table class="table thead-inverse">
+								<thead>
+									<tr>
+										<th width="150px">Action</th>
+										<th>Name</th>
+										<th>Comment</th>
+										<th>Link</th>
+									</tr>
+								</thead>
+								<tbody>
+									<?php
+										while($Result = mysqli_fetch_array($res)) {
+									?>
+									<tr>
+										<td><?php echo $Result['Name'];?></div></td>
+                    <td><?php echo $Result['Comment'];?></td>
+                    <td><a href="delete.php?ID=<?php echo $Result['ID'];?>" class="btn btn-sm btn-primary">ลบ</a> 
+                        <a href="edit.php?ID=<?php echo $Result['ID'];?>" class="btn btn-sm btn-primary">แก้ไข</a></td>
+									</tr>
+									<?php
+										}
+									?>
+								</tbody>
+							</table>
+							<?php
+								mysqli_close($conn);
+							?>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
 </body>
 </html>
