e58783d2c4116d6111a0674551f28384dca7baf3
63070129 <73679698+63070129@users.noreply.github.com>
2020-11-20 15:29:45 +0700

    Update insert_new.php

--- insert_new.php
+++ insert_new.php
@@ -3,13 +3,12 @@
 	<title>Editor</title>
 </head>
 <body>
-  <form action = "update.php?id='.$Result['ID'].'" method = "post" id="CommentForm" >
+<?php<form action = " update.php?id='.$Result['ID'].'" method = "post" id="CommentForm" >?>
     Name:<br>
-    <input type="text" name="name" id=<?php echo $Result['name'];?>><br>
+    <input type="text" name = "name" value="<?php echo  $Result['name'];?>" id="idName" placeholder="Enter Name"><br>
     Comment:<br>
-    <textarea rows="10" cols="20" name = "comment" id=<?php echo $Result['name'];?>></textarea><br>
+    <textarea rows="10" cols="20" name = "comment" value="<?php echo  $Result['comment'];?> id="idComment" placeholder="Enter Comment"></textarea><br>  
     <input type="submit" id="commentBtn">
   </form>
-
 </body>
 </html>
