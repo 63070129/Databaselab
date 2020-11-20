f92d388dfe37f0c002a8a5eb7c737e3c4315e551
63070129 <73679698+63070129@users.noreply.github.com>
2020-11-20 23:47:32 +0700

    Update update.php

--- update.php
+++ update.php
@@ -1,4 +1,3 @@
-'labpc129.mysql.database.azure.com', 'it63070129@labpc129', '029154897zZ', 'itflab');
 <?php
 
 $conn = mysqli_init();
@@ -15,13 +14,39 @@ $link = $_POST['link'];
 
 
 $sql = "UPDATE guestbook SET Name = '$name', Comment = '$comment', Link = '$link' WHERE ID = $id";
-
-
-if (mysqli_query($conn, $sql)) {
-    echo "New record created successfully";
-  } else {
-    echo "Error: " . $sql . "<br>" . mysqli_error($conn);
-  }
-  
-mysqli_close($conn);
-?>
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Edit Zone</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css">
+	<style type="text/css">
+		.card {
+			border-top: solid 5px #37cf23;
+		}
+	</style>
+</head>
+<body class="bg-light py-5">
+	<div class="container text-dark">
+		<div class="row">
+			<div class="col-12 col-lg-8 offset-lg-2">
+				<div class="card shadow">
+					<div class="card-body">
+						<h2 align="center">
+						<?php
+							if(mysqli_query($conn, $sql)) {
+								echo "ADD COMPLETED";
+							}
+							else {
+								echo "FAILED TO ADD";
+							}
+						?>
+						</h2>
+						<p align="center" class="mt-4 mb-0"><a href="show.php" class="btn btn-sm btn-success">BACK</a></p>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</body>
+</html>
