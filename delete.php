9c4a38c5374b9cdc8ca78e4ace63a19f9d9d8674
63070129 <73679698+63070129@users.noreply.github.com>
2020-11-20 21:46:53 +0700

    Update delete.php

--- delete.php
+++ delete.php
@@ -3,14 +3,15 @@ $conn = mysqli_init();
 mysqli_real_connect($conn, 'labpc129.mysql.database.azure.com', 'it63070129@labpc129', '029154897zZ', 'itflab', 3306);
 if (mysqli_connect_errno($conn))
 {
-die('Failed to connect to MySQL: '.mysqli_connect_error());
+    die('Failed to connect to MySQL: '.mysqli_connect_error());
 }
-$id = $_GET['id'];
-$sql = "DELETE FROM guestbook WHERE id = $id";
+$id = $_GET['ID'];
+$sql = "DELETE FROM guestbooks WHERE id = $id";
+
 if (mysqli_query($conn, $sql)) {
-echo "Record deleted successfully";
-} else {
-echo "Error: " . $sql . "<br>" . mysqli_error($conn);
-}
-mysqli_close($conn);
+    echo "Record deleted successfully";
+    } else {
+    echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+    }
+    mysqli_close($conn);
 ?>
